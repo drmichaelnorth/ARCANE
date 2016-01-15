fbfb0ddae6f4b1cac1c025ccedce2b8d9b6a3a13
drmichaelnorth <drmichaelnorth>
2016-01-15 17:20:36 -0600

    Added supporting files to the index.

---
+++ scenarios/gov/anl/cue/arcane/engine/matrix/ScenarioRuns.java
@@ -0,0 +1,184 @@
+/*
+ * Copyright © 2016, UChicago Argonne, LLC
+ * All Rights Reserved
+ * ARCANE (ANL-SF-15-108)
+ * Michael J. North, Argonne National Laboratory
+ * Pam Sydelko, Argonne National Laboratory
+ * Ignacio Martinez-Moyano
+ * 
+ * OPEN SOURCE LICENSE
+ * 
+ * Under the terms of Contract No. DE-AC02-06CH11357 with UChicago
+ * Argonne, LLC, the U.S. Government retains certain rights in this
+ * software.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1.	Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer. 
+ * 2.	Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ * 3.	Neither the names of UChicago Argonne, LLC or the Department of Energy
+ *      nor the names of its contributors may be used to endorse or promote
+ *      products derived from this software without specific prior written
+ *      permission. 
+ *  
+ * ****************************************************************************
+ * DISCLAIMER
+ * 
+ * THE SOFTWARE IS SUPPLIED “AS IS” WITHOUT WARRANTY OF ANY KIND.
+ * 
+ * NEITHER THE UNTED STATES GOVERNMENT, NOR THE UNITED STATES DEPARTMENT OF
+ * ENERGY, NOR UCHICAGO ARGONNE, LLC, NOR ANY OF THEIR EMPLOYEES, MAKES ANY
+ * WARRANTY, EXPRESS OR IMPLIED, OR ASSUMES ANY LEGAL LIABILITY OR
+ * RESPONSIBILITY FOR THE ACCURACY, COMPLETENESS, OR USEFULNESS OF ANY
+ * INFORMATION, DATA, APPARATUS, PRODUCT, OR PROCESS DISCLOSED, OR REPRESENTS
+ * THAT ITS USE WOULD NOT INFRINGE PRIVATELY OWNED RIGHTS.
+ * 
+ ******************************************************************************
+ *
+ * @author Michael J. North
+ * @version 1.1.0
+ * 
+*/
+package gov.anl.cue.arcane.engine.matrix;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+import gov.anl.cue.arcane.engine.Util;
+import gov.anl.cue.arcane.engine.matrix.MatrixEngine;
+
+/**
+ * The Class ScenarioRuns.
+ */
+public class ScenarioRuns {
+
+	/**
+	 * Instantiates a new scenario runs.
+	 */
+	public ScenarioRuns() {
+	}
+	
+	/**
+	 * Small scenario baseline run.
+	 *
+	 * @throws IOException Signals that an I/O exception has occurred.
+	 */
+	@Test
+	public void smallScenarioBaselineRun() throws IOException {
+
+		// Test matrix model template import.
+		MatrixEngine matrixEngine = MatrixEngine
+				.importTemplate(Util.INPUT_DIR +
+				"//MatrixEngineTemplate_SmallScenario_ParameterEvolution.xlsx");
+		
+		// Perform a constant run.
+		System.out.println(
+				matrixEngine.outputPopulation.get(0).getFitnessValue());
+		
+	}
+
+	/**
+	 * Small scenario parameter evolution run.
+	 *
+	 * @throws IOException Signals that an I/O exception has occurred.
+	 */
+	@Test
+	public void smallScenarioParameterEvolutionRun() throws IOException {
+
+		// Test matrix model template import.
+		MatrixEngine matrixEngine = MatrixEngine
+				.importTemplate(Util.INPUT_DIR +
+				"//MatrixEngineTemplate_SmallScenario_ParameterEvolution.xlsx");
+		
+		// Perform a stochastic run.
+		matrixEngine.stochasticRun(Util.OUTPUT_DIR +
+				"//MatrixEngineTemplate_SmallScenario_ParameterEvolution",
+				30, 500);
+		
+	}
+
+	/**
+	 * Small scenario equation evolution run.
+	 *
+	 * @throws IOException Signals that an I/O exception has occurred.
+	 */
+	@Test
+	public void smallScenarioEquationEvolutionRun() throws IOException {
+
+		// Test matrix model template import.
+		MatrixEngine matrixEngine = MatrixEngine
+				.importTemplate(Util.INPUT_DIR +
+				"//MatrixEngineTemplate_SmallScenario_EquationEvolution.xlsx");
+		
+		// Perform a stochastic run.
+		matrixEngine.stochasticRun(Util.OUTPUT_DIR +
+				"//MatrixEngineTemplate_SmallScenario_EquationEvolution",
+				30, 500);
+		
+	}
+
+	/**
+	 * Large scenario baseline run.
+	 *
+	 * @throws IOException Signals that an I/O exception has occurred.
+	 */
+	@Test
+	public void largeScenarioBaselineRun() throws IOException {
+
+		// Test matrix model template import.
+		MatrixEngine matrixEngine = MatrixEngine
+				.importTemplate(Util.INPUT_DIR +
+				"//MatrixEngineTemplate_LargeScenario_ParameterEvolution.xlsx");
+		
+		// Perform a constant run.
+		System.out.println(
+				matrixEngine.outputPopulation.get(0).getFitnessValue());
+		
+	}
+
+	/**
+	 * Large scenario parameter evolution run.
+	 *
+	 * @throws IOException Signals that an I/O exception has occurred.
+	 */
+	@Test
+	public void largeScenarioParameterEvolutionRun() throws IOException {
+
+		// Test matrix model template import.
+		MatrixEngine matrixEngine = MatrixEngine
+				.importTemplate(Util.INPUT_DIR +
+				"//MatrixEngineTemplate_LargeScenario_ParameterEvolution.xlsx");
+		
+		// Perform a stochastic run.
+		matrixEngine.stochasticRun(Util.OUTPUT_DIR +
+				"//MatrixEngineTemplate_LargeScenario_ParameterEvolution",
+				30, 500);
+		
+	}
+
+	/**
+	 * Large scenario equation evolution run.
+	 *
+	 * @throws IOException Signals that an I/O exception has occurred.
+	 */
+	@Test
+	public void largeScenarioEquationEvolutionRun() throws IOException {
+
+		// Test matrix model template import.
+		MatrixEngine matrixEngine = MatrixEngine
+				.importTemplate(Util.INPUT_DIR +
+				"//MatrixEngineTemplate_LargeScenario_EquationEvolution.xlsx");
+		
+		// Perform a stochastic run.
+		matrixEngine.stochasticRun(Util.OUTPUT_DIR +
+				"//MatrixEngineTemplate_LargeScenario_EquationEvolution",
+				30, 500);
+		
+	}
+
+}
